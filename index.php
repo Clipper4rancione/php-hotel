9c545a59273541b3341c699770a2a82480232ecc
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-01 17:51:43 +0100

    printed all data in table

--- index.php
+++ index.php
@@ -40,6 +40,9 @@
 
     ];
 
+    $hotel = $hotels[0];
+
+    //var_dump($hotel);
 ?>
 
 <!DOCTYPE html>
@@ -48,19 +51,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
     <title>PHP Hotel</title>
 </head>
 <body>
     <div class="container">
         <ul>
         <?php     
-            foreach($hotels as $hotelList){
-                foreach($hotelList as $key => $hotel){
-                    echo "<li>$key: $hotel</li>";
-                }
-            }            
+            
+            
         ?> 
         </ul>
+        <table class="table">
+  <thead>
+    <tr>
+    <?php foreach($hotel as $key => $value) :?>
+        <th scope="col"><?php echo $key ?></th>
+        <?php endforeach; ?>
+    </tr>
+
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) :?>
+        <tr>
+        <?php foreach($hotel as $key => $value) :?>
+            <td><?php echo $value ?></td>
+        <?php endforeach; ?>
+        </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
     </div>
 </body>
 </html>
